b00d11acc07eeff08a59e31ac7eee7ee0ab8f0e7
P3026236 <P3026236@CORTOPSAL1114T5.CORP.CHARTERCOM.com>
2021-07-10 18:56:35 -0500

    refactor

--- src/main/java/com/ak/dependency/Pipeline.java
+++ src/main/java/com/ak/dependency/Pipeline.java
@@ -1,9 +1,11 @@
 package com.ak.dependency;
 
+import com.ak.aspect.MethodLog;
 import com.ak.dependency.model.BalanceInfo;
 
 public class Pipeline {
-
+	
+	@MethodLog
 	public BalanceInfo balanceInfo() {
 
 		BalanceInfo balanceInfo = new BalanceInfo();
--- src/main/java/com/ak/dependency/Route.java
+++ src/main/java/com/ak/dependency/Route.java
@@ -1,12 +1,14 @@
 package com.ak.dependency;
 
+import com.ak.aspect.MethodLog;
 import com.ak.dependency.model.AccountInfo;
 import com.ak.dependency.model.BalanceInfo;
 
 public class Route {
-
+	
+	@MethodLog
 	public AccountInfo accountInfo() {
-
+		
 		AccountInfo accountInfo = new AccountInfo();
 		accountInfo.setAccountNo("123456");
 		accountInfo.setAccountType("Checking");
